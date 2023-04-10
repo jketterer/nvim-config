8105b1f5e31c66977e60eec3af771053e413503f
Jeremy Ketterer <jeremy.s.ketterer.ctr@army.mil>
2023-04-10 11:08:33 -0400

    add extra keybinds for comment toggle

--- init.lua
+++ init.lua
@@ -133,7 +133,9 @@ local plugins = {
         "terrortylor/nvim-comment",
         keys = {
             { "<C-/>", "<cmd>CommentToggle<cr>" },    
+            { "<C-S-/>", "<cmd>CommentToggle<cr>j" },    
             { "<C-/>", "<C-\\><C-N><cmd>CommentToggle<cr>i", mode = { "i" } },    
+            { "<C-S-/>", "<C-\\><C-N><cmd>CommentToggle<cr>ji", mode = { "i" } },    
         },
         main = "nvim_comment",
         config = true
